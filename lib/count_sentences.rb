ae27126b46790ac288ae24165e2e1562e4ddefa2
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-17 18:45:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.!?]/).count
-    new_array.reject {|s| s.empty?}
-    new_array
+    new_array = self.split(/[.!?]/).reject {|s| s.empty?}
+    new_array.count
   end
 end
